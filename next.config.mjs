3582a7387447ece183b7c9a714de487bb3c710e3
fredbrock <fredbrock2018@outlook.com>
2024-07-14 14:14:50 +0800

    change: initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
+import { withContentlayer } from "next-contentlayer2";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withContentlayer(nextConfig);
---
+++ posts/post-02.md
@@ -0,0 +1,6 @@
+---
+title: My First Post2
+date: 2021-12-22
+---
+
+Ullamco et nostrud magna commodo nostrud ...
